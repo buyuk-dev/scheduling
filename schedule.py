28fa2340d49bd003ebb8359a37db2854d2dae5c9
Michal Michalski <buyuk.dev@gmail.com>
2019-10-27 21:34:10 +0100

    Print task stream for each processor.

--- schedule.py
+++ schedule.py
@@ -3,6 +3,7 @@
 
 import sys
 import argparse
+from pprint import pprint
 from matplotlib import pyplot as plt
 
 
@@ -13,7 +14,15 @@ class Task:
         d - due time
     """
 
+    _next_id = 0
+
+    @classmethod
+    def next_id(cls):
+        cls._next_id += 1
+        return cls._next_id - 1
+
     def __init__(self, r, p, d):
+        self.id = self.next_id()
         self.r = r
         self.p = p
         self.d = d
@@ -26,6 +35,9 @@ class Task:
 
     def __str__(self):
         return f"{self.r} {self.p} {self.d}"
+
+    def __repr__(self):
+        return f"<{self.id}>"
 
     @classmethod
     def from_string(cls, string):
@@ -74,20 +86,38 @@ def plot_schedule(tasks):
     plt.show()
 
 
+def print_schedule(tasks):
+    """ Print scheduling.
+    """
+    proc_to_tasks = {}
+    for t in tasks:
+        if t.pid not in proc_to_tasks:
+            proc_to_tasks[t.pid] = [t]
+        else:
+            proc_to_tasks[t.pid].append(t)
+
+    for pid in proc_to_tasks:
+        proc_to_tasks[pid] = sorted(proc_to_tasks[pid], key=lambda t: t.start)
+
+    pprint(proc_to_tasks)
+
+
 def main(opts):
+    """ Main.
+    """
     scheduled = schedule(Instance.from_file(opts.input))
     opts.input.close()
 
     delay = sum(t.delay() for t in scheduled)
     print(f"Total delay: {delay}")
 
+    print_schedule(scheduled)
     if opts.plot:
         plot_schedule(scheduled)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-
     parser.add_argument(
         "input",
         type=argparse.FileType("r"),
@@ -95,6 +125,5 @@ if __name__ == "__main__":
         default=sys.stdin,
         help="input file path. stdin will be used if ommited",
     )
-
     parser.add_argument("--plot", action="store_true", help="show Gantt chart")
     main(parser.parse_args())
