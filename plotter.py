893f3260cb54c137addf11c7ebf082165259e62a
Michal Michalski <buyuk.dev@gmail.com>
2019-12-01 18:10:06 +0100

    Added plotter script for instances from file.

---
+++ plotter.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import argparse
+from matplotlib import pyplot
+from instance import Instance
+
+
+def plot_instance(instance):
+    """ show tasks on a graph
+    """
+    for k, t in enumerate(instance.tasks):
+        x = [t.r, t.r + t.p, t.d]
+        y = [k] * len(x)
+        pyplot.plot(x, y, marker="x")
+    pyplot.show()
+
+
+def plot_scheduling(instance, scheduling_path):
+    """ plot scheduling for given instance
+    """
+    pass
+
+
+def main(args):
+    instance = None
+    with open(args.instance, "r") as instance_file:
+        instance = Instance.from_file(instance_file)
+
+    if args.instance:
+        plot_instance(instance)
+
+    elif args.scheduling:
+        plot_scheduling(instance, args.scheduling)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--instance", help="instance filename")
+    parser.add_argument("--scheduling", help="scheduling filename")
+    args = parser.parse_args()
+    main(args) 
