43c408fac2c33fa33eec7cccf9e257fd42a59a84
Michal Michalski <buyuk.dev@gmail.com>
2019-12-12 22:17:30 +0100

    Updated run script.

--- run.sh
+++ run.sh
@@ -5,8 +5,17 @@ index=$1
 
 function compute {
     n=$1
-    value=$(cat "instances/${index}/in${index}_${n}.txt" | ./schedule.py --eval)
-    echo $value
+    instance="instances/${index}/in${index}_${n}.txt"
+    solution="output/out${index}_${n}.txt"
+
+    value=$(./schedule.py --algorithm urgent $instance)
+    runtime=$(echo "$value" | grep "duration")
+    scheduling=$(echo "$value" | grep -v "duration")
+
+    echo "$scheduling" > $solution
+
+    validate=$(./validate.py --instance $instance --solution $solution)
+    echo "$index_$n: $runtime $validate"
 }
 
 
